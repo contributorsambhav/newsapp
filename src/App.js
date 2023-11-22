136423ad8a6ef0e9b60666d0c8a3ed55450dddbe
contributorsambhav <Sambhav511974@gmail.com>
2023-11-22 21:20:41 +0530

    Created a navbar using bootstrap and class based components used

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-
+import Navbar from './components/Navbar';
 import React, { Component } from 'react'
 //news api key 06454ab3934b4aea83409ae14d4825ba
+
+
 export default class App extends Component {
-  c = "John"
-  render() {
-    return (
-      <div>
-        Hello my first class based component {this.c}
-      </div>
-    )
+  render(){
+   return(
+    <>
+    <Navbar></Navbar>
+    </>
+   )
   }
 }
