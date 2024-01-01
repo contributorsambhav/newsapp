ce072b9d6a1c65ff6e1ea056c42c7899b1b19367
contributorsambhav <sambhav511974@gmail.com>
2024-01-01 18:30:23 +0530

    Newsapp restored

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import "./App.css";
-
-import ChessBoard from "./components/ChessBoard";
-import LabelTop from "./components/LabelTop";
-import NumLabel from "./components/NumLabel";
-
-import React, { Component } from "react";
-import ChessPiece from "./components/chessPiece";
+import './App.css';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import React, { Component } from 'react'
 //news api key 06454ab3934b4aea83409ae14d4825ba
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
 
-        <div className="ChessBox">
-          <LabelTop></LabelTop>
-          <div className="horizontal">
-            <NumLabel />
-          </div>
-          <div className="horizontal">
-            <ChessBoard></ChessBoard>
-          </div>
-          <div className="horizontal">
-            <NumLabel></NumLabel>
-          </div>
-          <LabelTop></LabelTop>
-        </div>
-        <div className="chess-piece">
-        <ChessPiece pieceType={"K"} color={"white"} />
-        </div>
-      </>
-    );
+export default class App extends Component {
+  render(){
+   return(
+    <>
+    <Navbar></Navbar>
+    <News></News>
+    </>
+   )
   }
 }
